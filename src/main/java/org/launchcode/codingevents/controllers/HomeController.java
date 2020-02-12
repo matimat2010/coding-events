82a6c5911c8f9dcad43884e32294d1c08599ba43
Matt Alvarado <matimat2010@gmail.com>
2020-02-11 18:43:33 -0600

    11.10 Exercises Thymeleaf

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Created by Chris Bay
+ * Created by Chris Bay Updated by Matthew Alvardo: Hash Map, Table, and Bootstrap Added
  */
 @Controller
 public class HomeController {
